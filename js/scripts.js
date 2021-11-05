41787e30cb246e22bd5539bf47ccece43161f2c5
RosarioRu <mariarruval@gmail.com>
2021-11-04 20:21:42 -0700

    Add branching function for resulting different languages for three (height, color, animal) dummy/placeholder conditions.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,24 @@ $(document).ready(function() {
     const userHeight=$("input#height").val();
     const userColor =$("input#color").val();
     const userAnimal=$("input#animal").val();
-    const userAmPm=$("input#am-pm").val();
-    const userFood = $("input#food").val();
+    // const userAmPm=$("input#am-pm").val();
+    // const userFood = $("input#food").val();
+
+    // let result;
+    // if (userColor === "red" || userColor==="Red" || userColor==="red." || userColor==="Red." ) {
+    //   result="Ruby";
+    // }
 
     let result;
-    if (userColor === "red" || userColor==="Red" || userColor==="red." || userColor==="Red." ) {
+    if (userHeight === "tall") {
+      result="Python";
+    } else if (userColor === "red") {
       result="Ruby";
+    } else if (userAnimal === "cats") {
+      result = "C++";
+    }
+      else {
+      result = "nothing!";
     }
 
     $(".results-here").text(result);
