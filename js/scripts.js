1f0cb6e6c98c8629392461c2d3a13a25d34de567
RosarioRu <mariarruval@gmail.com>
2021-11-06 12:15:45 -0700

    Add other conditions to branching function and update with updated labels/names.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   $("form#language-suggester").submit(function(event) {
-    const developmentType=$("input#dev-type").val();
-    const userColor =$("input#color").val();
-    const userAnimal=$("input#animal").val();
+    const userMathematician=$("input#math").val();
+    const userFavoriteApp =$("input#favorite-app").val();
+    const userDevType=$("input#development-type").val();
+    const userEase=$("input#easiest").val();
+    const userFrontOrBackDev=$("input#front-or-back").val();
+
     // const userAmPm=$("input#am-pm").val();
     // const userFood = $("input#food").val();
 
@@ -10,17 +13,32 @@ $(document).ready(function() {
     // if (userColor === "red" || userColor==="Red" || userColor==="red." || userColor==="Red." ) {
     //   result="Ruby";
     // }
-
     let result;
-    if (developmentType === "video game" || developmentType === "video games") {
-      result="C++";
-    } else if (userColor === "red") {
-      result="Ruby";
-    } else if (userAnimal === "cats") {
+    if (userMathematician === "yes" || "Yes" || "Yes." || "yes." || "I am" || "i am" || "I am." || "i am." || "yep" || "Yep") {
+      result = "Haskell";
+    } else if (userFavoriteApp === iOS Enlight Photo Editor || Sky Guide Constellation Finder) {
+      result = "Swift";
+    } else if (userDevType === Game Development) {
       result = "C++";
+    } else if (userEase === yes) {
+      result= "Python";
+    } else if (userFrontOrBackDev === front) {
+      result = "Javascript";
     } else {
-      result = "nothing!";
+      result = "Ruby";
     }
+
+    
+    // let result;
+    // if (developmentType === "video game" || developmentType === "video games") {
+    //   result="C++";
+    // } else if (userColor === "red") {
+    //   result="Ruby";
+    // } else if (userAnimal === "cats") {
+    //   result = "C++";
+    // } else {
+    //   result = "nothing!";
+    // }
 
     $(".results-here").text(result);
     $(".results-here").show();
